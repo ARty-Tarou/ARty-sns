2429233131cd442f1dc0d687427e5808c8dbd214
Yusuke <now.good.1206@gmail.com>
2020-04-16 14:55:34 +0900

    a

---
+++ ARty/ARty/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ARty
+//
+//  Created by 今吉佑介 on 2020/04/15.
+//  Copyright © 2020 Swift-Beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func email(_ sender: Any) {
+    }
+    
+    
+    @IBAction func password(_ sender: Any) {
+    }
+    
+    @IBAction func decisin(_ sender: Any) {
+    }
+    
+    
+    @IBAction func signin(_ sender: Any) {
+    }
+    
+}
+
