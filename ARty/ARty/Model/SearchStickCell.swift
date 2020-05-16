cb6b1bed83a9c980d0f0301f420b658b85d42dce
Krenoa <mtpgadmt@gmail.com>
2020-05-16 22:32:51 +0900

    0516 final commit

---
+++ ARty/ARty/Model/SearchStickCell.swift
@@ -0,0 +1,19 @@
+//
+//  SearchStickCell.swift
+//  ARty
+//
+//  Created by 藤崎伊織 on 2020/05/16.
+//  Copyright © 2020 Swift-Beginners. All rights reserved.
+//
+
+import UIKit
+
+class SearchStickCell: UICollectionViewCell{
+    
+    @IBOutlet weak var stickImageView: UIImageView!
+    @IBOutlet weak var userIconImageView: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var overViewTextView: UITextView!
+    @IBOutlet weak var goodButton: UIButton!
+    @IBOutlet weak var numberOfGood: UILabel!
+}
--- ARty/ARty/ViewController/SearchViewController.swift
+++ ARty/ARty/ViewController/SearchViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 import NCMB
 
-class SearchViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionViewDataSource, UICollectionViewDelegate {
     
     // MARK: Properties
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var leftTableView: UITableView!
     @IBOutlet weak var rightTableView: UITableView!
+    @IBOutlet weak var leftCollectionView: UICollectionView!
+    @IBOutlet weak var rightCollectionView: UICollectionView!
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,42 +28,87 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UITableViewDe
         // ナビゲーションバーを非表示にする
         navigationController?.setNavigationBarHidden(true, animated: true)
         
-        // TableViewの設定
-        leftTableView.register(UINib(nibName: "SearchCell", bundle: nil), forCellReuseIdentifier: "searchCell")
-        rightTableView.register(UINib(nibName: "SearchCell", bundle: nil), forCellReuseIdentifier: "searchCell")
-        leftTableView.delegate = self
-        rightTableView.delegate = self
-        leftTableView.dataSource = self
-        rightTableView.dataSource = self
+        // CollectionViewの設定
+        leftCollectionView.register(UINib(nibName: "SearchStickCell", bundle: nil), forCellWithReuseIdentifier: "searchStickCell")
+        rightCollectionView.register(UINib(nibName: "SearchStickCell", bundle: nil), forCellWithReuseIdentifier: "searchStickCell")
+        leftCollectionView.delegate = self
+        rightCollectionView.delegate = self
+        leftCollectionView.dataSource = self
+        rightCollectionView.dataSource = self
+        
+        // Cellのレイアウトを設定
+        let leftLayout = UICollectionViewFlowLayout()
+        let rightLayout = UICollectionViewFlowLayout()
+        leftLayout.itemSize = CGSize(width: 182, height: 290)
+        rightLayout.itemSize = CGSize(width: 182, height: 290)
+        leftCollectionView.collectionViewLayout = leftLayout
+        rightCollectionView.collectionViewLayout = rightLayout
+        
         
         // デリゲートを設定
         searchTextField.delegate = self
+        
     }
     
     // MARK: Codable
     
     
+    
+    
     // MARK: Method
-    func pullSearch(){
+    
+    // 検索する文字列
+    var searchWord: String? = nil
+    func pullSearchStampStick(){
         // スクリプトインスタンスを生成
-        let script = NCMBScript(name: "pullSearch.js", method: .post)
+        let script = NCMBScript(name: "pullSearchStampStick.js", method: .post)
         // ボディを設定
-        let requestBody: [String: Any?]  = ["userId": NCMBUser.currentUser?.objectId, "word": searchTextField.text]
+        let requestBody: [String: Any?]  = ["searchWord": searchWord]
         // スクリプトを実行
         script.executeInBackground(headers: [:], queries: [:], body: requestBody, callback: {result in
             switch result{
             case let .success(data):
-                print("pullSearchScript実行に成功:\(String(data: data ?? Data(), encoding: .utf8) ?? "")")
+                print("pullSearchStampScript実行に成功:\(String(data: data ?? Data(), encoding: .utf8) ?? "")")
                 
                 
             case let .failure(error):
-                print("pullSearchScript実行に失敗:\(error)")
+                print("pullSearchStampScript実行に失敗:\(error)")
             }
         })
     }
     
+    func pullSearchStampArtStick(){
+        // スクリプトインスタンスを生成
+        let script = NCMBScript(name: "pullSearchStampArtStick.js", method: .post)
+        // ボディを設定
+        let requestBody: [String: Any?] = ["searchWord": searchWord]
+        // スクリプトを実行
+        script.executeInBackground(headers: [:], queries: [:], body: requestBody, callback: {result in
+            switch result{
+            case let .success(data):
+                print("pullSearchStampArtStickScript実行に成功:\(String(data: data ?? Data(), encoding: .utf8) ?? "")")
+                
+                
+            case let .failure(error):
+                print("pullSearchStampArtStickScript実行に失敗:\(error)")
+            }
+        })
+    }
     
-    // MARK: Delegate methods
+    // MARK: Action
+    @IBAction func searchButtonAction(_ sender: Any) {
+        if searchTextField.text == ""{
+            print("検索文字列が入力されてないよ")
+        }else{
+            print("検索するよ:\(String(describing: searchTextField.text))")
+            searchWord = searchTextField.text
+            pullSearchStampStick()
+            pullSearchStampArtStick()
+        }
+    }
+    
+    
+    // MARK: UITextField
     
     // テキストフィールドでリターンが押されたときに呼ばれる
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -70,46 +120,53 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UITableViewDe
             print("検索：\(str)")
         }
         // MARK: リクエスト処理
-        // TODO: 検索処理のスクリプトを記述していく。
+        // TODO: 検索処理のスクリプトを記述していく。と思ったけど検索ボタンおしたらに変えた。
 
         
         return true
     }
     
-    // セルの総数を返す
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // スタンプリストの総数
-        
-        if tableView == leftTableView{
-            return 5 //leftTableViewのセルの数
-        }else{
-            return 10 //rightTableViewのセルの数
-        }
-    }
+    // MARK: UICollectionViewDataSource
     
-    // セルの高さ
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 350
-    }
-    
-    // セルに値を設定する
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // 表示するCellを取得
-        let cell = tableView.dequeueReusableCell(withIdentifier: "searchCell") as! SearchCell
+    // セルを返す
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "searchStickCell", for: indexPath) as! SearchStickCell
         
-        if tableView == leftTableView{
-            cell.SearchUserName.text = "left"
-            cell.SearchUserImage.image = UIImage(named: "FirstIcon")
-            cell.SearchImage.image = UIImage(named: "urety")
-            cell.SearchDescribe.text = "概要"
+        // セルに情報を付加
+        if collectionView == leftCollectionView{
+            // Stampのセル
+            cell.goodButton.tag = indexPath.row
+            print("left:\(indexPath.row)")
+            
+            cell.userName.text = "left"
         }else{
-            cell.SearchUserName.text = "right"
-            cell.SearchUserImage.image = UIImage(named: "FirstIcon")
-            cell.SearchImage.image = UIImage(named: "muty")
-            cell.SearchDescribe.text = "概要"
+            // StampArtのセル
+            cell.goodButton.tag = indexPath.row
+            print("right:\(indexPath.row)")
+            
+            cell.userName.text = "right"
         }
+        
         
         return cell
     }
+    
+    // セル数を返す
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == leftCollectionView{
+            // Stampのセル
+            return 5
+        }else{
+            // Stampのセル
+            return 10
+        }
+    }
+    
+    // セルが選択されたとき
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    
 
 }
